bbbac8cbc126d548c12be8867d209ae7760d675e
IvoCorrea <ivoaurelio30@gmail.com>
2025-05-20 06:22:27 -0300

    first commit

---
+++ src/main/java/com/java10x/cursoSpringFiasco/CursoSpringFiascoApplication.java
@@ -0,0 +1,13 @@
+package com.java10x.cursoSpringFiasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringFiascoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringFiascoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java10x/cursoSpringFiasco/CursoSpringFiascoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java10x.cursoSpringFiasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringFiascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
